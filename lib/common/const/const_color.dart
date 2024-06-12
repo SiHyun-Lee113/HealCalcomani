6dd28bc3c477294f23fc3ac9572a95bd5c02f421
isihyeon <tlgus0113@gmail.com>
2024-06-12 23:40:32 +0900

    [feat] 상수값 관리

---
+++ lib/common/const/const_color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color PRIMARY_COLOR = Color(0xFF000000);
+const Color BODY_100_COLOR = Color(0xFFFFFFFF);
+const Color BODY_70_COLOR = Color(0xFF747474);
+const Color BODY_50_COLOR = Color(0xFF444444);
+const Color NAGATIVE_COLOR = Color(0xFFFF0000);
